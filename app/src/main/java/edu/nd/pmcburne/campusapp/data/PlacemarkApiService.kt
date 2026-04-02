ee3641535d4c562136f5da2e1ad8bb320aeae968
RishiSarraff <sarraff2004@gmail.com>
2026-04-02 17:42:25 -0400

    Completed HW Assignment

--- app/src/main/java/edu/nd/pmcburne/campusapp/data/PlacemarkApiService.kt
+++ app/src/main/java/edu/nd/pmcburne/campusapp/data/PlacemarkApiService.kt
@@ -10,17 +10,16 @@ import java.util.concurrent.TimeUnit
 
 private val json = Json { ignoreUnknownKeys = true }
 
-private val client = OkHttpClient.Builder()
-    .connectTimeout(15, TimeUnit.SECONDS)
-    .readTimeout(15, TimeUnit.SECONDS)
-    .build()
-
 interface PlacemarkApiService {
     @GET("placemarks.json")
     suspend fun getPlacemarks(): List<PlacemarkResponse>
 }
 
 object PlacemarkApi {
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS)
+        .readTimeout(15, TimeUnit.SECONDS)
+        .build()
     private val retrofit = Retrofit.Builder()
         .baseUrl("https://www.cs.virginia.edu/~wxt4gm/")
         .client(client)
